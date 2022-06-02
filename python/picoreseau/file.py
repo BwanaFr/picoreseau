5cb36fb0623497d0e50c7ea83b4a1c43491c7ae1
Mathieu Donze <mathieu.donze@cern.ch>
2022-06-02 08:15:45 +0200

    WIP on Nanoreseau files

--- python/picoreseau/file.py
+++ python/picoreseau/file.py
@@ -1,6 +1,133 @@
 import struct
 from datetime import date
 import os
+import sys
+from tabnanny import NannyNag
+
+
+class BinaryCode :
+    """
+        Represents a binary code segment
+    """
+
+    def __init__(self, code_address, data, code_page=None):
+        """
+            Constructor
+
+            Parameters
+            ----------
+            code_address : int
+                Memory address of the code location
+            data : bytesarray
+                Code data
+            code_page : int
+                Memory page of the code location
+        """
+        self.code_address = code_address
+        self.data = data
+        self.code_page = code_page
+
+    def __str__(self):
+        ret = f'Binary code of {len(self.data)} bytes at ${self.code_address:04x}'
+        if self.code_page is not None:
+            if self.code_page == 0:
+                ret += ' (no page change)'
+            else:
+                ret += f' on page {self.code_page & ~0x80}'
+        return ret
+
+class BinaryData :
+    """
+        Binary data
+    """
+
+    REC_SIMPLE_CODE = 0x0       # Simple binary record
+    REC_SIMPLE_EXEC_ADDR = 0xff        # Simple execution address binary record
+    REC_EXTENDED = 0x1          # Extended binary record
+    REC_EXT_CODE = 0x2          # Extended binary code
+    REC_EXT_EXEC_ADDR = 0x3     # Extended binary execution address
+
+    # Structures definition
+    REC_SIMPLE_CODE_STRUCT = '>HH'
+    REC_SIMPLE_CODE_STRUCT_LEN = struct.calcsize(REC_SIMPLE_CODE_STRUCT)
+    REC_SIMPLE_EXEC_ADDR_STRUCT = '>2xH'
+    REC_SIMPLE_EXEC_ADDR_STRUCT_LEN = struct.calcsize(REC_SIMPLE_EXEC_ADDR_STRUCT)
+    REC_EXTENDED_STRUCT = '>BBBH32s'
+    REC_EXTENDED_STRUCT_LEN = struct.calcsize(REC_EXTENDED_STRUCT)
+    REC_EXT_CODE_STRUCT = '>HHB'
+    REC_EXT_CODE_STRUCT_LEN = struct.calcsize(REC_EXT_CODE_STRUCT)
+    REC_EXT_ADDR_STRUCT = '>HHB'
+    REC_EXT_ADDR_STRUCT_LEN = struct.calcsize(REC_EXT_ADDR_STRUCT)
+
+    def __init__(self, file=None):
+        self.machine_type = None
+        self.code_language = None
+        self.loading_byte = None
+        self.app_name = None
+        self.bin_code = []
+        self.exec_address = None
+        self.exec_page = None
+        if file:
+            self.__read_binary(file)
+
+    def __add_binary_code(self, code : BinaryCode):
+        self.bin_code.append(code)
+
+    def __read_binary(self, f):
+        while True:
+            reg_type = f.read(1)
+            if not reg_type:
+                #EOF reached
+                break
+            # Gets record type
+            reg_type = int.from_bytes(reg_type, 'little')
+            if reg_type == BinaryData.REC_SIMPLE_CODE:
+                # Simple binary code record
+                code_len, code_addr = struct.unpack_from(BinaryData.REC_SIMPLE_CODE_STRUCT, f.read(BinaryData.REC_SIMPLE_CODE_STRUCT_LEN))
+                code = BinaryCode(code_addr, f.read(code_len))
+                self.__add_binary_code(code)
+            elif reg_type == BinaryData.REC_SIMPLE_EXEC_ADDR:
+                # Simple binary code execution address
+                self.exec_address, = struct.unpack_from(BinaryData.REC_SIMPLE_EXEC_ADDR_STRUCT, f.read(BinaryData.REC_SIMPLE_EXEC_ADDR_STRUCT_LEN))
+                # This is the last record in the binary file
+                break
+            elif reg_type == BinaryData.REC_EXTENDED:
+                # Binary extended file description
+                self.machine_type, self.code_language, self.loading_byte, nul_bytes, app_name = \
+                                struct.unpack_from(BinaryData.REC_EXTENDED_STRUCT, f.read(BinaryData.REC_EXTENDED_STRUCT_LEN))
+                self.app_name = app_name.decode('utf-8')
+            elif reg_type == BinaryData.REC_EXT_CODE:
+                # Binary extended code
+                code_len, code_addr, code_page = struct.unpack_from(BinaryData.REC_EXT_CODE_STRUCT, f.read(BinaryData.REC_EXT_CODE_STRUCT_LEN))
+                code = BinaryCode(code_addr, f.read(code_len), code_page)
+                self.__add_binary_code(code)
+            elif reg_type == BinaryData.REC_EXT_EXEC_ADDR:
+                # Binary extended execution address
+                nul_bytes, exec_addr, exec_page = struct.unpack_from(BinaryData.REC_EXT_ADDR_STRUCT, f.read(BinaryData.REC_EXT_ADDR_STRUCT_LEN))
+                if nul_bytes != 0x0:
+                    print("Null bytes expected!")
+                self.exec_address = exec_addr
+                self.exec_page = exec_page
+                # This is the last record in the binary file
+                break
+
+    def __str__(self):
+        ret =  f'File binary'
+        if self.app_name:
+            ret += f' (extended) application {self.app_name}'
+        else:
+            ret += ' (simple)'
+        ret += f' starting at ${self.exec_address:04x} '
+        if self.exec_page is not None:
+            if self.exec_page == 0:
+                ret += '(no page change)'
+            else:
+                ret += f'on page {self.exec_page & ~0x80}'
+
+        for c in self.bin_code:
+            ret += '\n' + str(c)
+        return ret
+
 
 class NanoreseauFile:
     """
@@ -32,6 +159,7 @@ class NanoreseauFile:
         self.created_on = None          # File creation computer (0 : TO7, 1 : MO5, 2 : TO7/70)
         self.creation_language = None   # File creation language (0 : N.C., 1 : BASIC, 2 : LOGO, 3 : LSE)
         self.app_bytes = bytearray(48)  # Applications bytes
+        self.binary_data = None         # File binary data
         if file_path:
             self.open_file(file_path)
 
@@ -42,7 +170,11 @@ class NanoreseauFile:
             year = 2000 + year
         else:
             year = 1900 + year
-        return date(year,int.from_bytes(b[1:2], 'little'),int.from_bytes(b[2:3], 'little'))
+        month = int.from_bytes(b[1:2], 'little')
+        day = int.from_bytes(b[2:3], 'little')
+        if month == 0 or day == 0:
+            return None
+        return date(year, month, day)
 
     def get_file_type(self):
         """
@@ -95,15 +227,28 @@ class NanoreseauFile:
         # Skip 46 bytes
         f.seek(46, os.SEEK_CUR)
         f.readinto(self.app_bytes)
-        print(f'Actual file position {f.tell()}')
-        print(f'Next byte is {int.from_bytes(f.read(1), "little")}')
-    
+        if self.file_mode == NanoreseauFile.FILE_MODE_BINARY and self.type == NanoreseauFile.FILE_TYPE_MACHINE:
+            self.binary_data = BinaryData(f)
+        elif self.type == NanoreseauFile.FILE_TYPE_INDEXED:
+            # Indexed files are not yet supported
+            pass
+        else:
+            # Other data type, read all bytes
+            self.binary_data = f.read()
+
     def __str__(self):
-        return f'File ID : {self.identifier} v{self.file_version_major}.{self.file_version_minor} {self.creation_date}, modified {self.modification_date}'
+        ret = f'File ID : {self.identifier} v{self.file_version_major}.{self.file_version_minor} {self.creation_date}, modified {self.modification_date} \
+           \nFile type : {self.get_file_type()} Status : {self.get_file_status()}'
+        if self.binary_data:
+            ret += f'\n' + str(self.binary_data)
+        return ret
 
 if __name__ == "__main__":
-    f = NanoreseauFile("MENU.MO5")
+    f = None
+    if len(sys.argv) > 1:
+        f = NanoreseauFile(sys.argv[1])
+    else:
+        f = NanoreseauFile("MENU.MO5")
     print(str(f))
-    print(f'File type : {f.get_file_type()} Status : {f.get_file_status()}')
 
 
